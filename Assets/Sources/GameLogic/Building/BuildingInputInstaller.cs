dbc5aace830ed77a79068f4c17e03988ece5f51f
AliceMon <xachwork2@gmail.com>
2022-10-27 21:33:34 +0300

    next

---
+++ Assets/Sources/GameLogic/Building/BuildingInputInstaller.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Sources.BuildingLogic
+{
+    public class BuildingInputInstaller : MonoInstaller
+    {
+        [SerializeField] private BuildingInput _buildingInput;
+
+        public override void InstallBindings()
+        {
+            Container.Bind<IBuildingInput>().FromInstance(_buildingInput).AsSingle();
+        }
+    }
+}
