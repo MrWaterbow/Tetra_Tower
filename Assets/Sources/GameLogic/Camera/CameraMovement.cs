e467604511b4955fb3821f4b414e476e80c6b0f6
Gezuriya <pelmen7892@gmail.com>
2022-12-02 12:02:59 +0300

    Camera Rotation

--- Assets/Sources/GameLogic/Camera/CameraMovement.cs
+++ Assets/Sources/GameLogic/Camera/CameraMovement.cs
@@ -1,5 +1,6 @@
 ﻿using DG.Tweening;
 using Sources.BuildingLogic;
+using System;
 using UnityEngine;
 using Zenject;
 
@@ -16,10 +17,18 @@ namespace Sources.CameraLogic
 
         [Space]
 
-        [SerializeField] private Transform _camera;
+        [SerializeField] private Transform _cameraTransform;
         [SerializeField] private Transform _root;
 
         private BuildingInstaller _buildingInstaller;
+
+        [Space]
+
+        [SerializeField] private Camera _camera;
+        [SerializeField] private float _sensetivity;
+        [SerializeField] Transform _rotationPoint;
+
+        private Vector3 _previousPos;
 
         [Inject]
         private void Construct(BuildingInstaller buildingInstaller)
@@ -29,7 +38,7 @@ namespace Sources.CameraLogic
 
         private void Awake()
         {
-            _camera.position = _root.position + _offset;
+            _cameraTransform.position = _root.position + _offset;
         }
 
         private void OnEnable()
@@ -44,14 +53,27 @@ namespace Sources.CameraLogic
 
         private void OnValidate()
         {
-            _camera.position = _root.position + _offset;
+            _cameraTransform.position = _root.position + _offset;
         }
 
         private void MoveCamera()
         {
             _offset.y += _moveOffset;
-
-            _camera.DOMove(_root.position + _offset, _moveOffset);
+            //_camera.transform.position = new Vector3(transform.position.x, _root.position.y+_offset.y, transform.position.z);
+            _cameraTransform.DOMove(_root.position + _offset, _moveOffset);
+        }
+       
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                _previousPos = _camera.ScreenToViewportPoint(Input.mousePosition);
+            }
+            if (Input.GetMouseButton(0))
+            {
+                Vector3 direction = _previousPos - _camera.ScreenToViewportPoint(Input.mousePosition);
+                _camera.transform.RotateAround(_rotationPoint.position, Vector3.up, -direction.x * _sensetivity);
+            }
         }
     }
 }
