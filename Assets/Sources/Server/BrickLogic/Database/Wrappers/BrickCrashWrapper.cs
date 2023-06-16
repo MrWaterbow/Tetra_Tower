23a698e95f07088887abade2336f9fdb2f3a6759
AliceMon <xachwork2@gmail.com>
2023-06-16 11:17:27 +0300

    foot-factor

--- Assets/Sources/Server/BrickLogic/Database/Wrappers/BrickCrashWrapper.cs
+++ Assets/Sources/Server/BrickLogic/Database/Wrappers/BrickCrashWrapper.cs
@@ -1,4 +1,6 @@
-﻿namespace Server.BrickLogic
+﻿using UnityEngine;
+
+namespace Server.BrickLogic
 {
     public sealed class BrickCrashWrapper
     {
@@ -7,6 +9,29 @@
         public BrickCrashWrapper(BricksDatabase database)
         {
             _database = database;
+        }
+
+        public void CheckBricksCrashing()
+        {
+
+        }
+
+        public float ComputeFootFactor(Brick brick)
+        {
+            float stableTilesCount = 0;
+
+            foreach (Vector3Int tile in brick.Pattern)
+            {
+                Vector3Int tilePosition = brick.Position + tile;
+                Vector2Int heightMapKey = new(tilePosition.x, tilePosition.z);
+
+                if(_database.GetHeightByKey(heightMapKey) == tilePosition.y && _database.Surface.PositionInSurfaceLimits(heightMapKey))
+                {
+                    stableTilesCount++;
+                }
+            }
+
+            return stableTilesCount / brick.Pattern.Length;
         }
     }
 }
